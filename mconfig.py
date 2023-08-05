046c8aad0e7ec3886b70eee3fade63d24f853a93
Amit Kumar Manjhi <42512407+commityourdream@users.noreply.github.com>
2023-08-05 18:49:56 +0530

    config file

---
+++ mconfig.py
@@ -0,0 +1,13 @@
+# mconfig.py
+
+# PostgreSQL database credentials
+DB_HOST = 'your_postgres_host'
+DB_PORT = 'your_postgres_port'
+DB_NAME = 'your_database_name'
+DB_USER = 'your_database_user'
+DB_PASSWORD = 'your_database_password'
+
+# Outlook email credentials
+SENDER_EMAIL = 'your_email@example.com'
+SENDER_PASSWORD = 'your_email_password'
+RECEIVER_EMAILS = ['recipient1@example.com', 'recipient2@example.com', 'recipient3@example.com']
